4190f737f022d33d7e527f7bc02ae1d4088d1a22
Александр <113357408+Mr-cmd7@users.noreply.github.com>
2022-10-11 16:04:47 +0200

    Add files via upload

---
+++ Lab 3.2/Lab 3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿// 22-ИСП-2/1 Саидбаев А.П. 7 вариант уровень высокий 
+
+try
+{
+    double number, summa=0;
+    Console.WriteLine("Для поиска суммы отрицательных чисел закончите ввод значений, значением : 0");
+    Console.WriteLine("Введите значения через Enter: ");
+    do
+    {
+        number = double.Parse(Console.ReadLine());
+
+        if (number > 0) continue;
+
+        summa += number;
+    } while (number != 0);
+    Console.WriteLine("Сумма отрицательных чисел: "+summa);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
